3d66f8463a529baee974be5b21f185e7d3567c7e
Todd Malsbary <todd.malsbary@intel.com>
2021-02-02 16:50:52 -0800

    WIP prereqs
    Signed-off-by: Todd Malsbary <todd.malsbary@intel.com>
    Change-Id: Ie2e116b9a157255a2a59dafd8e8fcaaebba7a181

--- kud/hosting_providers/vagrant/setup.sh
+++ kud/hosting_providers/vagrant/setup.sh
@@ -11,7 +11,7 @@
 set -o nounset
 set -o pipefail
 
-vagrant_version=2.2.4
+vagrant_version=2.2.14
 if ! vagrant version &>/dev/null; then
     enable_vagrant_install=true
 else
@@ -94,7 +94,6 @@ case ${ID,,} in
     ;;
 
     ubuntu|debian)
-    libvirt_group="libvirtd"
     INSTALLER_CMD="sudo -H -E apt-get -y -q=3 install"
     packages+=(python-dev)
 
@@ -176,7 +175,7 @@ sudo modprobe vhost_net
 
 ${INSTALLER_CMD} "${packages[@]}"
 if ! which pip; then
-    curl -sL https://bootstrap.pypa.io/get-pip.py | sudo python
+    curl -sL https://bootstrap.pypa.io/2.7/get-pip.py | sudo python
 else
     sudo -H -E pip install --no-cache-dir --upgrade pip
 fi
